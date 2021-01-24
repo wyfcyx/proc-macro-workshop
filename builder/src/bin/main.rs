63d00f5df5f271a78374140599de8ad2d08adc51
Yifan Wu <shinbokuow@163.com>
2021-01-25 01:59:19 +0800

    Upload.

---
+++ builder/src/bin/main.rs
@@ -0,0 +1,15 @@
+use derive_builder::Builder;
+
+#[derive(Builder)]
+pub struct Command {
+    executable: String,
+    args: Vec<String>,
+    env: Vec<String>,
+    current_dir: String,
+}
+
+fn main() {
+    let builder = Command::builder();
+
+    let _ = builder;
+}
--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,51 @@
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream, Span};
+use syn::{
+    self,
+    DeriveInput,
+    Ident,
+    Data,
+    DataStruct,
+};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = syn::parse(input).unwrap();
+    impl_builder_macro(&ast)
+}
 
-    unimplemented!()
+fn impl_builder_macro(ast: &DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let builder_name = Ident::new(
+        format!("{}Builder", name).as_str(),
+        name.span(),
+    );
+    if let Data::Struct(data_struct) = &ast.data {
+        let fields = data_struct.fields.iter();
+        /*
+        let mut field_idents_v: Vec<Ident> = Vec::new();
+        fields.for_each(|f| {
+            field_idents_v.push(f.ident.unwrap());
+        });
+        let field_idents = field_idents_v.iter();
+         */
+        let field_idents = fields.map(|f| {
+            f.ident.unwrap()
+        }).iter();
+
+        let gen = quote! {
+            pub struct #builder_name {
+                #(#fields ,)*
+            }
+            impl #name {
+                fn builder() -> #builder_name {
+                    #(#field_idents : None,)
+                }
+            }
+        };
+        gen.into()
+    } else {
+        panic!("The type is not a struct!");
+    }
+
 }
